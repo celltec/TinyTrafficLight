c6d4559b66750f5ff96bfaf7e1505fa9b55c6bba
celltec <celltec@users.noreply.github.com>
2019-08-01 15:47:14 +0200

    Add some comments and rename 'doAmpelThings' to something serious :)

--- main.c
+++ main.c
@@ -16,45 +16,43 @@
 /* Funktionsprototypen */
 uint16_t readAnalog(void);
 void delay(uint16_t ms);
-void doAmpelThings(uint8_t enableDelay);
+void stepTrafficLight(uint8_t enableDelay);
 void blink(uint16_t ms);
 
 int main(void)
 {
 	/* Timer initialisieren */
-	TCCR0A = 0x0;                  // Timer in normal mode initialisieren
-	TCCR0B |= (1 << CS02);         // Pre-scaler: 256
+	TCCR0A = 0x0;                           // Timer in normal mode initialisieren
+	TCCR0B |= (1 << CS02);                  // Pre-scaler: 256
 
 	/* ADC initialisieren */
-	ADMUX |= (1 << REFS0) | (1 << MUX2);     // Referenzspannung AVcc und Kanal 4
-	ADCSRA |= (1 << ADEN);                   // ADC Aktivieren
-	ADCSRA |= (1<<ADSC);                     // Erst einsatzbereit nach einmaligem Lesen
-	while (ADCSRA & (1 << ADSC));            // "dummy read"
-	(void)ADCW;                              // Ergebnis ignorieren
+	ADMUX |= (1 << REFS0) | (1 << MUX2);    // Referenzspannung AVcc und Kanal 4
+	ADCSRA |= (1 << ADEN);                  // ADC Aktivieren
+	ADCSRA |= (1 << ADSC);                  // Erst einsatzbereit nach einmaligem Lesen
+	while (ADCSRA & (1 << ADSC));           // "dummy read"
+	(void)ADCW;                             // Ergebnis ignorieren
 
 	/* GPIO initialisieren */
-	DDRB  = 0b00001110;            // Ein und Ausgänge definieren
-	PORTB = 0b00110000;            // Interne Pull-Up Widerstände aktivieren
+	DDRB = 0b00001110;                      // Ein und Ausgänge definieren
+	PORTB = 0b00110000;                     // Interne Pull-Up Widerstände aktivieren
 
 	while (1)
 	{
-		while (READ(JUMPER))
+		while (READ(JUMPER))                // Modus prüfen: Jumper drin -> manueles Schalten
 		{
-			if (READ(BUTTON))
+			if (READ(BUTTON))               // Knopfdruck prüfen
 			{
-				doAmpelThings(0);
-				delay(150);
+				stepTrafficLight(0);        // Licht um eine Position weiter stellen
+				delay(150);                 // Einfaches Entprellen und maximale Schaltgeschwindigkeit
 			}
 		}
 
-		if (READ(BUTTON))
-		{
+		while (READ(BUTTON))                // Gelb blinken, solange der Knopf gedrückt wird (ohne Jumper)
+		{	                                // Die Blinkgeschwindigkeit richtet sich nach dem Potentiometer
 			blink(readAnalog() << 2UL);     // Shift nach links um die Zeit mit vier zu multiplizieren
 		}
-		else
-		{
-			doAmpelThings(1);
-		}
+
+		stepTrafficLight(1);                // Ansonsten die Ampel automatisch schalten lassen
 	}
 
 	return 0;
@@ -63,10 +61,10 @@ int main(void)
 uint16_t readAnalog(void)
 {
 	uint16_t val;
-	ADCSRA |= (1 << ADSC);            // Konvertierung starten
-	while (ADCSRA & (1 << ADSC));     // Warten bis Werte gemessen wurden
-	val = (uint16_t)ADCL;             // Low lesen (Bits 1 bis 8)
-	val |= (ADCH << 8);               // High lesen (Bit 9 und 10)
+	ADCSRA |= (1 << ADSC);                  // Konvertierung starten
+	while (ADCSRA & (1 << ADSC));           // Warten bis Werte gemessen wurden
+	val = (uint16_t)ADCL;                   // Low lesen (Bits 1 bis 8)
+	val |= (ADCH << 8);                     // High lesen (Bit 9 und 10)
 	return val;
 }
 
@@ -74,12 +72,12 @@ void delay(uint16_t ms)
 {
 	while (ms--)
 	{
-		TCNT0 = 209;       // Vorladen
-		while (TCNT0);     // Auf Überlauf warten
+		TCNT0 = 209;                        // Vorladen
+		while (TCNT0);                      // Auf Überlauf warten
 	}
 }
 
-void doAmpelThings(uint8_t enableDelay)
+void stepTrafficLight(uint8_t enableDelay)
 {
 	static uint8_t step = 0;
 
